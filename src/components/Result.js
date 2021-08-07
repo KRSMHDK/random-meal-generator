b60bfc4fb767fdefac448f259e3b6383c668b785
Kris <kris.mahadhika@gmail.com>
2021-08-07 11:06:58 +0800

    complete

--- src/components/Result.js
+++ src/components/Result.js
@@ -9,14 +9,16 @@ function Result({ food }) {
     const ingredient = [];
     for (let i = 1; i <= 20; i++) {
       let ingredient1 = 'strIngredient' + i;
+      let measure1 = 'strMeasure' + i;
 
       if (food.meals[0][ingredient1]) {
-        ingredient.push(food.meals[0][ingredient1]);
+        ingredient.push([food.meals[0][ingredient1], food.meals[0][measure1]]);
       } else {
         break;
       }
     }
     setIngredients(ingredient);
+    console.log(ingredient);
   }, [food]);
 
   return (
@@ -36,10 +38,16 @@ function Result({ food }) {
             <b>Area: </b> {food.meals[0].strArea}
           </p>
 
+          <p>
+            <b>Tags: </b> {food.meals[0].strTags ? food.meals[0].strTags : '-'}
+          </p>
+
           <h2>Ingredients:</h2>
           <ul>
             {ingredients.map((n) => (
-              <li>{n}</li>
+              <li>
+                {n[0]} - {n[1]}
+              </li>
             ))}
           </ul>
         </div>
@@ -51,6 +59,14 @@ function Result({ food }) {
           </Typography>
         </div>
       </div>
+      <iframe
+        width='1280'
+        height='800'
+        title={food.meals[0].strMeal}
+        src={`https://www.youtube.com/embed/${food.meals[0].strYoutube.slice(
+          -11
+        )}`}
+      ></iframe>
     </div>
   );
 }
